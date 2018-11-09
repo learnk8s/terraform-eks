cbd145856e4a31edbbd3841343d401bb628977c3
Eric Ho <dho.eric@gmail.com>
2018-11-09 08:59:10 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform for EKS
+A script to spin up EKS cluster in AWS, including worker nodes
---
+++ variables.tf
@@ -0,0 +1,49 @@
+## General
+variable "aws_region" {
+    description = "The AWS region to build network infrastructure"
+    type        = "string"
+}
+
+variable "project" {
+    description = "Project name"
+    type        = "string"
+    default     = "learnk8s"
+}
+
+variable "env" {
+    description = "Environment of infrastructure"
+    type        = "string"
+    default     = "dev"
+}
+
+variable "extra_tags" {
+    description = "Extra AWS tags to be applied to created resources."
+    type        = "map"
+    default     = {}
+}
+
+## Netorking
+variable "aws_az_number" {
+    description = "How many AZs want to be used"
+    type        = "string"
+    default     = "3"
+}
+variable "vpc_cidr_block" {
+    description = "CIDR block for VPC"
+    type        = "string"
+    default     = "10.0.0.0/16"
+}
+
+
+## EKS master
+variable "config_output_path" {
+    description = "The path to store config, e.g. kubeconfig"
+    type = "string"
+    default = ".terraform"
+}
+
+variable "lb_sg_ids" {
+    description = "The security group id which used by load balancer"
+    type        = "list"
+    default     = []
+}
